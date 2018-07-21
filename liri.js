518ae4b85c9c1eb33bb40a138cbce17b31138384
disconaked <nolan.klinke@gmail.com>
2018-07-21 10:57:10 -0400

    added js

--- liri.js
+++ liri.js
@@ -2,6 +2,28 @@ require("dotenv").config();
 
 var keys = require("./keys.js")
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 
-var client = new Twitter(keys.twitter);
+//var client = new Twitter(keys.twitter);
+
+var input = process.argv[2];
+var nodeArgs = process.argv;
+var search = "";
+
+if (input == "my-tweets") {
+
+    console.log("this is where tweets will go");
+
+} else if (input == "spotify-this-song" || input == "movie-this") {
+
+    for (var i = 3; i < nodeArgs.length; i++) {
+        search += nodeArgs[i] + " ";
+    };
+
+    console.log(search);
+
+} else if (input == "do-what-it-says") {
+
+    console.log("do what it says");
+
+};
