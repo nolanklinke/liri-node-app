533b0159bef61f5515819827d47ef26750073311
disconaked <nolan.klinke@gmail.com>
2018-07-23 10:19:52 -0400

    api work

--- liri.js
+++ liri.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 
+var fs = require('fs');
+
+
 var keys = require("./keys.js")
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
-//var client = new Twitter(keys.twitter);
 
 var input = process.argv[2];
 var nodeArgs = process.argv;
@@ -14,10 +19,40 @@ if (input == "my-tweets") {
 
     console.log("this is where tweets will go");
 
-} else if (input == "spotify-this-song" || input == "movie-this") {
+    /*client.get('statuses/lookup', function(error, tweets, response) {
+        if(error) throw error;
+        console.log(tweets);  // The favorites.
+        console.log(response);  // Raw response object.
+      });*/
+    
+
+} else if (input == "spotify-this-song") {
 
     for (var i = 3; i < nodeArgs.length; i++) {
         search += nodeArgs[i] + " ";
+    };
+        
+    console.log("Searching for " + search);
+    
+
+    spotify
+    .search({ type: 'track', query: search, limit: 1 })
+    .then(function(response) {
+        var data = response;
+        console.log(JSON.stringify(data, null, 2));
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+           
+          
+
+
+} else if (input == "movie-this") {
+
+    for (var i = 3; i < nodeArgs.length; i++) {
+        search += nodeArgs[i] + " ";
+
     };
 
     console.log(search);
